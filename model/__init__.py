c23040b27444493cc87dee6a286bb55086d67948
zenglab-pku <101234901+zenglab-pku@users.noreply.github.com>
2024-11-27 02:45:12 +0800

    Add files via upload

---
+++ model/__init__.py
@@ -0,0 +1,6 @@
+from .config import add_DISSECT_config
+from .detector import DISSECT
+from .dataset_mapper import DISSECTDatasetMapper
+from .test_time_augmentation import DISSECTWithTTA
+from .swintransformer import build_swintransformer_fpn_backbone
+from .segmentation import segmentation
